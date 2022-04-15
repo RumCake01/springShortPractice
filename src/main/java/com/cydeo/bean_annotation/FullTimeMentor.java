ec35f90552578e8bfa9387584d011fa9a89856a4
RumCake01 <mia.hasnutdinova@gmail.com>
2022-04-15 00:11:52 -0400

    ApplicationContext and Bean factory review

---
+++ src/main/java/com/cydeo/bean_annotation/FullTimeMentor.java
@@ -0,0 +1,11 @@
+package com.cydeo.bean_annotation;
+
+import org.springframework.stereotype.Component;
+
+    @Component
+    public class FullTimeMentor {
+
+        public void createAccount() {
+            System.out.println("Full Time Mentor is created");
+        }
+        }
